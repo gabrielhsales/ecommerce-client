5a54f329f68d7c56182c65e289f6954090488c4e
Thauan Santos <nero.rbr@gmail.com>
2019-03-07 23:50:30 -0300

    fixed home title

--- src/store.js
+++ src/store.js
@@ -1,7 +1,7 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
+import Vue from 'vue'
+import Vuex from 'vuex'
 
-Vue.use(Vuex);
+Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
@@ -128,119 +128,117 @@ export default new Vuex.Store({
       openCheckoutModal: false
     }
   },
-  
+
   getters: {
     productsAdded: state => {
       return state.products.filter(el => {
-        return el.isAddedToCart;
-      });
+        return el.isAddedToCart
+      })
     },
     productsAddedToFavourite: state => {
       return state.products.filter(el => {
-        return el.isFavourite;
-      });
+        return el.isFavourite
+      })
     },
     getProductById: state => id => {
-      return state.products.find(product => product.id == id);
+      return state.products.find(product => product.id == id)
     },
     isUserLoggedIn: state => {
-      return state.userInfo.isLoggedIn;
+      return state.userInfo.isLoggedIn
     },
     isUserSignedUp: state => {
-      return state.userInfo.isSignedUp;
+      return state.userInfo.isSignedUp
     },
     getUserName: state => {
-      return state.userInfo.name;
+      return state.userInfo.name
     },
     isLoginModalOpen: state => {
-      return state.systemInfo.openLoginModal;
+      return state.systemInfo.openLoginModal
     },
     isSignupModalOpen: state => {
-      return state.systemInfo.openSignupModal;
+      return state.systemInfo.openSignupModal
     },
     isCheckoutModalOpen: state => {
-      return state.systemInfo.openCheckoutModal;
+      return state.systemInfo.openCheckoutModal
     },
     quantity: state => {
-      return state.products.quantity;
+      return state.products.quantity
     }
   },
-  
+
   mutations: {
     addToCart: (state, id) => {
       state.products.forEach(el => {
         if (id === el.id) {
-          el.isAddedToCart = true;
+          el.isAddedToCart = true
         }
-      });
+      })
     },
     setAddedBtn: (state, data) => {
       state.products.forEach(el => {
         if (data.id === el.id) {
-          el.isAddedBtn = data.status;
+          el.isAddedBtn = data.status
         }
-      });
+      })
     },
     removeFromCart: (state, id) => {
       state.products.forEach(el => {
         if (id === el.id) {
-          el.isAddedToCart = false;
+          el.isAddedToCart = false
         }
-      });
+      })
     },
     removeProductsFromFavourite: state => {
       state.products.filter(el => {
-        el.isFavourite = false;
-      });
+        el.isFavourite = false
+      })
     },
     isUserLoggedIn: (state, isUserLoggedIn) => {
-      state.userInfo.isLoggedIn = isUserLoggedIn;
+      state.userInfo.isLoggedIn = isUserLoggedIn
     },
     isUserSignedUp: (state, isSignedUp) => {
-      state.userInfo.isSignedUp = isSignedUp;
+      state.userInfo.isSignedUp = isSignedUp
     },
     setHasUserSearched: (state, hasSearched) => {
-      state.userInfo.hasSearched = hasSearched;
+      state.userInfo.hasSearched = hasSearched
     },
     setUserName: (state, name) => {
-      state.userInfo.name = name;
+      state.userInfo.name = name
     },
     setProductTitleSearched: (state, titleSearched) => {
-      state.userInfo.productTitleSearched = titleSearched;
+      state.userInfo.productTitleSearched = titleSearched
     },
     showLoginModal: (state, show) => {
-      state.systemInfo.openLoginModal = show;
+      state.systemInfo.openLoginModal = show
     },
     showSignupModal: (state, show) => {
-      state.systemInfo.openSignupModal = show;
+      state.systemInfo.openSignupModal = show
     },
     showCheckoutModal: (state, show) => {
-      state.systemInfo.openCheckoutModal = show;
+      state.systemInfo.openCheckoutModal = show
     },
     addToFavourite: (state, id) => {
       state.products.forEach(el => {
         if (id === el.id) {
-          el.isFavourite = true;
+          el.isFavourite = true
         }
-      });
+      })
     },
     removeFromFavourite: (state, id) => {
       state.products.forEach(el => {
         if (id === el.id) {
-          el.isFavourite = false;
+          el.isFavourite = false
         }
-      });
+      })
     },
     quantity: (state, data) => {
       state.products.forEach(el => {
         if (data.id === el.id) {
-          el.quantity = data.quantity;
+          el.quantity = data.quantity
         }
-      });
+      })
     }
   },
-  
-  actions: {
 
-  }
-});
+  actions: {}
+})
